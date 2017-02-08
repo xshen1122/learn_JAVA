0049abff33c9d807706e76d389c1ef6afa604780
XueqinShen <xueqin.shen@outlook.com>
2017-02-08 15:01:16 +0800

    BufferedReader

--- AA01/src/package6/Test06.java
+++ AA01/src/package6/Test06.java
@@ -1,21 +1,49 @@
 package package6;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 
 public class Test06 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Before=======================");
-		myread();
-		mywrite();
-		System.out.println("\nAfter=========================");
-		myread();
+//		System.out.println("Before=======================");
+//		myread();
+//		mywrite();
+//		System.out.println("\nAfter=========================");
+//		myread();
+		myRedirect();
 
+	}
+	
+	public static void myRedirect() throws IOException{
+		PrintStream console = System.out;
+	
+		BufferedInputStream in = new BufferedInputStream(new FileInputStream("C:\\Workstation\\iOS\\Notes\\iOS_1.txt"));
+
+
+		PrintStream out = new PrintStream(new FileOutputStream("C:\\Workstation\\iOS\\Notes\\test.out"));
+
+		System.setIn(in);
+		System.setOut(out);
+		System.setErr(out);
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		while((s=br.readLine())!=null){
+			System.out.println(s);
+		}
+		out.close();
+		System.setOut(console);
+		
 	}
 	public static void mywrite(){
 		String Filename = "C:\\Workstation\\iOS\\Notes\\java_basic.txt";
